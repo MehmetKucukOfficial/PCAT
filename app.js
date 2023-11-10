d9983d6c44d00e00ff0e8e2a730441b13e046cbb
Mehmet Küçük <23761mehmetkucuk23761@gmail.com>
2023-11-10 22:52:55 +0300

    MongoDb database connection was added and redirection was made to the home page.

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-
+const path = require('path');
+const Photo = require('./models/photo');
 const app = express();
+
+//Connect to MongoDB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template engine
 app.set('view engine', 'ejs');
 
 //Middleware
-
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { 
+    photos });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
